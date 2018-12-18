b88e1a067a4308d00ecdf61e407337644d68e917
Hanifa <Hanifahotelwala@gmail.com>
2018-12-17 22:02:29 -0500

    Exercise log in progress

--- FeaturesActivity.java
+++ FeaturesActivity.java
@@ -1,13 +1,10 @@
 package com.example.android.splitfeatures;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class FeaturesActivity extends AppCompatActivity {
     private Button cameraButton;
