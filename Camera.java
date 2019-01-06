e895d7895d37da4faf1b7aeca9abe6f23f5e2bdf
Hanifa <Hanifahotelwala@gmail.com>
2019-01-06 01:20:16 -0500

    Camera working

--- Camera.java
+++ Camera.java
@@ -1,13 +1,50 @@
 package com.example.android.splitfeatures;
 
+import android.nfc.Tag;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 public class Camera extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String TAG = "Camera";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+       Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+       startActivityForResult(cameraIntent,REQUEST_IMAGE_CAPTURE);
+
+
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode ==REQUEST_IMAGE_CAPTURE){
+            Log.d(TAG, "done taking a photo");
+
+
+        }
     }
 }
+
+
